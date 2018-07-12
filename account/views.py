ab80e3f1ebd432e44127938d41e092e7f07671b2
Tacro Hashimoto <tacro@g.ucla.edu>
2018-07-12 15:50:58 +0900

    added pages for  user authentication

--- account/views.py
+++ account/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def signup(request):
+    return render(request, 'account/signup.html')
+
+def login(request):
+    return render(request, 'account/login.html')
+
+def logout(request):
+    return render(request, 'account/logout.html')
