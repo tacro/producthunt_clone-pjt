a68f13ffbe07acfab63a8fb51bb07e329bbaa237
Tacro Hashimoto <tacro@g.ucla.edu>
2018-07-12 16:18:03 +0900

    really primitive auth system

--- account/views.py
+++ account/views.py
@@ -1,10 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 def signup(request):
-    return render(request, 'account/signup.html')
+    if request.method == 'POST':
+        # he wants to signup
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'account/signup.html', {'error': 'Username has already been taken'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                auth.login(request, user)
+                return redirect('home')
+        else:
+                return render(request, 'account/signup.html', {'error': "Pass doesn't match"})
+    else:
+        # just access
+        return render(request, 'account/signup.html')
 
 def login(request):
     return render(request, 'account/login.html')
 
 def logout(request):
+    # TODO Need to route to homepage
+    # and don't forget to logout
     return render(request, 'account/logout.html')
