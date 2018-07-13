aa5f5944149a13022bc8c7ac09b7d3a259ef666a
Tacro Hashimoto <tacro@g.ucla.edu>
2018-07-14 07:09:18 +0900

    add create page

--- product/views.py
+++ product/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request, 'product/home.html')
+
+@login_required
+def create(request):
+    return render(request, 'product/create.html')
